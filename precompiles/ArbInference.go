b8de575dffb499029cdf130a22321eb94579db4d
Matthew Wang <matthewwang2020@u.northwestern.edu>
2023-12-15 18:34:48 -0500

    Vanna Inference Updates

--- precompiles/ArbInference.go
+++ precompiles/ArbInference.go
@@ -1,23 +1,39 @@
 package precompiles
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/ethereum/go-ethereum/rpc/inference"
 )
 
 // ArbInference provides precompiles that serve as an entrypoint for AI and ML inference on the Vanna Blockchain
 type ArbInference struct {
-	model  string
-	params string
+	Address addr // 0x11a
 }
 
-func (con ArbInference) InferCall(c ctx, evm mech) (string, error) {
+func (con *ArbInference) InferCall(c ctx, evm mech, input []byte) ([]byte, error) {
+	inputStr := string(input)
+	inputArray := strings.Split(inputStr, "-")
+	modelName := inputArray[0]
+	inputData := ""
+	for _, value := range inputArray[1:] {
+		inputData = inputData + value
+	}
+
 	rc := inference.NewRequestClient(5125)
 	tx := inference.InferenceTx{
-		Hash:   "0x123456789",
-		Model:  "QmXQpupTphRTeXJMEz3BCt9YUF6kikcqExxPdcVoL1BBhy",
-		Params: "[[0.002, 0.005, 0.004056685]]",
+		Hash:   c.caller.String() + "#" + strconv.FormatUint(evm.StateDB.GetNonce(c.caller), 10),
+		Model:  modelName,
+		Params: inputData,
 		TxType: "inference",
 	}
-	// Return value should be ("0.0013500629", nil)
-	return rc.Emit(tx)
+	result, err := rc.Emit(tx)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	byteValue := make([]byte, len(result))
+	copy(byteValue, result)
+	return byteValue, nil
 }
