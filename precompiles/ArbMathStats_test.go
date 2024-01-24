7c6e2cf61668e9aa2449a9fa118e874d6446c331
Adam Balogh <adambalogh@Adams-MacBook-Air.local>
2024-01-23 23:34:22 -0500

    tests

--- precompiles/ArbMathStats_test.go
+++ precompiles/ArbMathStats_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestArbMathStatsStandardDeviation(t *testing.T) {
+func TestArbMathStatsStandardDeviationIdenticalElements(t *testing.T) {
 	mathStats := ArbMathStats{}
 
 	data := []int32{1, 1, 1, 1, 1, 1}
@@ -13,5 +13,23 @@ func TestArbMathStatsStandardDeviation(t *testing.T) {
 
 	if stdev.Cmp(big.NewInt(0)) != 0 {
 		t.Errorf("Expected stdev of 0 for identical elements, got: %s", stdev)
+	}
+}
+
+func TestArbMathStatsStandardDeviation(t *testing.T) {
+	mathStats := ArbMathStats{}
+
+	data := []int32{1, 5, 10, 100}
+
+	stdev, _ := mathStats.stdDev(nil, nil, data, 4)
+	expected := big.NewInt(474763)
+	if stdev.Cmp(expected) != 0 {
+		t.Errorf("Incorrect stddev, expected %s, got %s", expected, stdev)
+	}
+
+	stdev, _ = mathStats.stdDev(nil, nil, data, 2)
+	expected = big.NewInt(4747)
+	if stdev.Cmp(expected) != 0 {
+		t.Errorf("Incorrect stddev, expected %s, got %s", expected, stdev)
 	}
 }
