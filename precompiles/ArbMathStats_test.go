ce118af23daf31716de20446a79c57bd45a79cd8
Adam Balogh <adambalogh@Adams-MacBook-Air.local>
2024-01-23 23:27:11 -0500

    fix test

--- precompiles/ArbMathStats_test.go
+++ precompiles/ArbMathStats_test.go
@@ -11,7 +11,7 @@ func TestArbMathStatsStandardDeviation(t *testing.T) {
 	data := []int32{1, 1, 1, 1, 1, 1}
 	stdev, _ := mathStats.stdDev(nil, nil, data, 0)
 
-	if stdev != big.NewInt(0) {
-		t.Errorf("Expected stdev of 0 for identical elements")
+	if stdev.Cmp(big.NewInt(0)) != 0 {
+		t.Errorf("Expected stdev of 0 for identical elements, got: %s", stdev)
 	}
 }
